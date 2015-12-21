684c4d5f33b17befe15e467b4fc657a5765efcbe
Brian Addicks <brian@addicks.us>
2015-12-21 14:48:11 -0500

    finished up services

--- src/cmdlets/Get-WgService.ps1
+++ src/cmdlets/Get-WgService.ps1
@@ -13,30 +13,42 @@ function Get-WgService {
 	$VerbosePrefix = "Get-WgService: "
 	
 	$IpRx = [regex] "(\d+\.){3}\d+"
-	
+		
+	$IpProtocols = @{}
+	$IpProtocols."6"  = "tcp"
+	$IpProtocols."17" = "udp"
+	$IpProtocols."0"  = "0"
+	$IpProtocols."47" = "GRE"
+	$IpProtocols."2"  = "IGMP"
+	$IpProtocols."50" = "ESP"
+	$IpProtocols."51" = "AH"
+	$IpProtocols."89" = "OSPF"
+	$IpProtocols."1"  = "ICMP"
+			
 	$ConfigContents = [xml]$ConfigContents
 	$ReturnObject   = @()
 	
-	foreach ($Group in $ConfigContents.profile.'address-group-list'.'address-group') {
-		$NewAddressGroup              = New-Object WatchGuardParser.AddressGroup
-		$NewAddressGroup.Name         = $Group.Name
-		$NewAddressGroup.Property     = $Group.Property
-		$NewAddressGroup.Description  = $Group.description
-		$ReturnObject                += $NewAddressGroup
+	foreach ($Service in $ConfigContents.profile.'service-list'.'service') {
+		$NewService              = New-Object WatchGuardParser.Service
+		$NewService.Name         = $Service.name
+		$NewService.Property     = $Service.property
+		$NewService.Description  = $Service.description
+		$NewService.ProxyType    = $Service.'proxy-type'
+		$NewService.IdleTimeout  = $Service.'idle-timeout'
+		$ReturnObject           += $NewService
 		
-		foreach ($Member in $Group.'addr-group-member'.member) {
+		foreach ($Member in $Service.'service-item'.member) {
+			$Protocol = $IpProtocols."$($Member.Protocol)"
+			if (!($Protocol)) { Write-Verbose "Protocol not found: $($Member.Protocol)" }
 			switch ($Member.Type) {
 				1 {
-					$NewAddressGroup.Members += $Member.'host-ip-addr' + '/32'
+					$NewService.Members += "$Protocol/" + $Member.'server-port'
 				}
 				2 {
-					$NewAddressGroup.Members += $Member.'ip-network-addr' + '/' + (ConvertTo-MaskLength $Member.'ip-mask')
-				}
-				3 {
-					$NewAddressGroup.Members += $Member.'start-ip-addr' + '-' + $Member.'end-ip-addr'
+					$NewService.Members += $Protocol + '/' + $Member.'start-server-port' + '-' + $Member.'end-server-port'
 				}
 				default {
-					Write-Verbose "$($NewAddressGroup.Name) member type ($($Member.Type)) not found"
+					Write-Verbose "$($NewService.Name) member type ($($Member.Type)) not found"
 				}
 			}
 		}
