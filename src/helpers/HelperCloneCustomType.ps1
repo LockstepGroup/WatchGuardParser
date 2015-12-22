5649ee81e27aaf3fde2818efbd54c73180cc07c7
Brian Addicks <brian@addicks.us>
2015-12-22 11:15:19 -0500

    HelperCloneCustomType

---
+++ src/helpers/HelperCloneCustomType.ps1
@@ -0,0 +1,33 @@
+function HelperCloneCustomType {
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$True,Position=0)]
+		[object]$Object,
+		
+		[Parameter(Mandatory=$False,Position=1)]
+		[array]$AddProperties
+	)
+	
+	$VerbosePrefix = "HelperCloneCustomType: "
+	
+	$Type         = $Object.GetType().FullName
+	$Properties   = $Object | gm -Type Property
+	$ValidProperties = @()
+	
+	foreach ($p in $Properties) {
+		$ValidProperties += "$($p.Name)"
+	}
+	
+	if ($AddProperties) {
+		$ValidProperties += $AddProperties
+	}
+	
+	$ReturnObject = "" | Select $ValidProperties
+	#$global:ValidProperties = $ValidProperties
+	
+	foreach ($p in $Properties) {
+		$ReturnObject."$($p.Name)" = $Object."$($p.Name)"
+	}
+	
+	return $ReturnObject
+}
