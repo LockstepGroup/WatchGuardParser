81ff56e39d64e5af0c9fbc8d82e8d4b64f747ec2
Brian Addicks <brian@addicks.us>
2015-12-22 13:27:36 -0500

    added Resolve-wgalias

--- src/cmdlets/Resolve-WgPolicy.ps1
+++ src/cmdlets/Resolve-WgPolicy.ps1
@@ -21,26 +21,7 @@ function Resolve-WgPolicy {
 	
 	foreach ($p in $Policies) {
 		if ($p.$Field) {
-			$Lookup = $LookupTable | ? { $_.Name -eq $p.$Field }
-			$NewPrefix = $Field -replace 'aliaslist',''
-			foreach ($m in $Lookup.Members) {
-				$NewProperties = @()
-				$MemberProperties = ($m | gm -type Property).Name
-				foreach ($prop in $MemberProperties) {
-#					if ($prop -match 'name') { continue }
-					$NewProperties += "$NewPrefix$prop"
-					Write-Verbose "$NewPrefix$prop"
-				}
-				
-				$NewPolicy = HelperCloneCustomType $p $NewProperties
-				$Global:NewPolicy = $NewPolicy
-				
-				foreach ($prop in $MemberProperties) {
-#					if ($prop -match 'name') { continue }
-					$NewPolicy."$NewPrefix$prop" = $m.$prop
-				}
-				$ReturnObject += $NewPolicy
-			}
+            
 		} else {
 			$ReturnObject += HelperCloneCustomType $p
 		}
