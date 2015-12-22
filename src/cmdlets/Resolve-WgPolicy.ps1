e30b90f24f09a3e1fa42c55597df606c4225d1c0
Brian Addicks <brian@addicks.us>
2015-12-22 13:37:12 -0500

    updated resolve-wgpolicy for new alias cmdlet

--- src/cmdlets/Resolve-WgPolicy.ps1
+++ src/cmdlets/Resolve-WgPolicy.ps1
@@ -21,7 +21,28 @@ function Resolve-WgPolicy {
 	
 	foreach ($p in $Policies) {
 		if ($p.$Field) {
-            
+            switch ($Field) {
+                { $_ -match "alias" } {
+                    $NewFieldPrefix = $Field -replace 'aliaslist',''
+                    
+                    $AliasFields = @("User"
+                                     "Address"
+                                     "Interface"
+                                     "AliasName")
+                    $NewFields = @()
+                    foreach ($a in $AliasFields) { $NewFields += "$NewFieldPrefix$a" }
+                    
+                    $ResolvedAlias = Resolve-WgAlias $p.$Field $LookupTable
+                    foreach ($Result in $ResolvedAlias) {
+                        $NewPolicy = HelperCloneCustomType $p $NewFields
+                        foreach ($a in $AliasFields) {
+                            $NewPolicy."$NewFieldPrefix$a" = $Result.$a
+                        }
+                        $ReturnObject += $NewPolicy
+                    }
+                }
+                default { Throw "VerbosePrefix Field not handled: $Field" }
+            }
 		} else {
 			$ReturnObject += HelperCloneCustomType $p
 		}
